0bef067faefdfe185856af1c501b98a658f4279c
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-19 19:55:08 +0100

    as

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
+import { appleImg, bagImg, searchImg } from "../utils"
 
 const Navbar = () => {
   return (
-    <div>
-      .
-    </div>
+    <header>
+      <nav>
+        <img src={appleImg} alt="apple" width={14} height={18} />
+      </nav>
+
+      <div>
+        {['Phones', 'Macbooks', 'Tablets',].map((nav) => (
+            <div key={nav}>
+                {nav}
+            </div>
+        ))}
+      </div>
+
+      <div>
+        <img src={searchImg} alt="search" width={18} height={18}/>
+        <img src={bagImg} alt="bag" width={18} height={18}/>
+      </div>
+    </header>
   )
 }
 
