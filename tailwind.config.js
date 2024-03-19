0d9a0c474252e8898d6cd9b72db76beaf00e6031
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-19 21:36:08 +0100

    jhgf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx.tsx}",
+    "./src/**/*.{html,js}",
   ],
   theme: {
     extend: {
