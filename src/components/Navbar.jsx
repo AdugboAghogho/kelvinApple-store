b4e726d98cd4b39af761f69a712b838b9056899c
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-19 20:34:48 +0100

    tgg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { navLists } from './../constants/index';
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-       <nav className="flex w-full screen-max-width">
+        <nav className="flex w-full screen-max-width">
             <img src={appleImg} alt="apple" width={14} height={18} />
 
             <div className="flex flex-1 justify-center max-sm:hidden">
@@ -19,7 +19,7 @@ const Navbar = () => {
                 <img src={searchImg} alt="search" width={18} height={18}/>
                 <img src={bagImg} alt="bag" width={18} height={18}/>
             </div>
-       </nav>
+        </nav>
     </header>
   )
 }
