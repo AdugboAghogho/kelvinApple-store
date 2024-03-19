b8642a88b199fcc27f0c7fb49934ebf99f8d85f4
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-19 21:49:27 +0100

    razs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { navLists } from '../constants';
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-white">
-      <nav className="flex w-full screen-max-width bg-white">
+      <nav className="flex w-full screen-max-width bg-white mt-10">
         <img src={appleImg} alt="Apple" width={14} height={18} />
 
         <div className="flex flex-1 justify-center max-sm:hidden bg-white">
