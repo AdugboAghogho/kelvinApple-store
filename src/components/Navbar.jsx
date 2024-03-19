d4acc77ad44861aae737436970845f0baae82179
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-19 21:35:07 +0100

    t5f

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-      <nav className="flex w-full screen-max-width">
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-white">
+      <nav className="flex w-full screen-max-width bg-white">
         <img src={appleImg} alt="Apple" width={14} height={18} />
 
-        <div className="flex flex-1 justify-center max-sm:hidden">
+        <div className="flex flex-1 justify-center max-sm:hidden bg-white">
           {navLists.map((nav) => (
             <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
               {nav}
