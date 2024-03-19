7dd0960a2429603bb3209e62e5831632720607eb
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-19 21:27:54 +0100

    awes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,25 @@
-import { appleImg, bagImg, searchImg } from "../utils"
-import { navLists } from './../constants/index';
+import { appleImg, bagImg, searchImg } from '../utils';
+import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-        <nav className="flex w-full screen-max-width">
-            <img src={appleImg} alt="Apple" width={14} height={18} />
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
 
-            <div className="flex flex-1 justify-center max-sm:hidden">
-                {navLists.map((nav) => (
-                    <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
-                        {nav}
-                    </div>
-                ))}
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+              {nav}
             </div>
+          ))}
+        </div>
 
-            <div>
-                <img src={searchImg} alt="search" width={18} height={18}/>
-                <img src={bagImg} alt="bag" width={18} height={18}/>
-            </div>
-        </nav>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
     </header>
   )
 }
