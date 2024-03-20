33945ac1aa3dad3c6b82bba9e8a4e4d9bc01378f
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-20 14:16:58 +0100

    uewiaydg

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,10 +1,29 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 
 
 const Highlights = () => {
+
+  useGSAP(() => {
+    gsap.to('#title', {
+      opacity: 1,
+      delay: 2,
+    })
+    gsap.to('#cta', { 
+      opacity: 1, 
+      y: -50, 
+      delay: 2 
+    })
+  })
+
   return (
-    <div>
-      .
-    </div>
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">Get The Highligths.</h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
