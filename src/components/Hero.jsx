7361aebe2e32b1cd1017c93f27f3d2d399b99661
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-20 13:32:26 +0100

    fsd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,15 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to('#hero', {
+      opacity: 1,
+      delay:1.5,
+    })
+  })
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
