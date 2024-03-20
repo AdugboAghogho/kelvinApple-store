b8ec95aa68cd6bd03689f4e36846c83f66161985
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-20 13:14:05 +0100

    asc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
-            <div key={nav} className="px-5 text-sm cursor-pointer text-gray  hover:scale-125 transition-all">
+            <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white  transition-all">
               {nav}
             </div>
           ))}
         </div>
 
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1 hover:scale-125 cursor-pointer">
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1  cursor-pointer">
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
