09d5bac2a05547b1ac21735a5134619cbee5af2c
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-20 13:37:21 +0100

    ,kjhv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">iPhone 15 Pro</p>
       </div>
     </section>
   )
