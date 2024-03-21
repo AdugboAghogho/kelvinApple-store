35ebb52509e04bedc8ca05be8e3632d1a05517bc
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-21 19:29:30 +0100

    asfzx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -34,7 +34,7 @@ const Model = () => {
     }, []);
 
   return (
-    <section className='common-paddin'>
+    <section className='common-padding'>
        <div className="screen-max-width">
             <h1 id='heading' className='section-heading'>
                 Take a closer look.
