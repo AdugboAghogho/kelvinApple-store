88b6c7cc811752cb9e8c19cde5a3fb19755b7d18
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-21 14:58:02 +0100

    ssd

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,9 +1,30 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import ModelView from './ModelView';
+import { yellowImg } from '../utils';
+import { c } from 'vite/dist/node/types.d-FdqQ54oU';
 
 const Model = () => {
+    const [size, setSize] = useState('small');
+    const [model, setModel] = useState({
+        title: 'iPhone 15 Pro in Natural Titanium',
+        color: ['#8f8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg,
+    });
+
+    // CAMERA CONTROL FOR THE MODEL VIEW
+    const cameraControlSmall = useRef();
+    const cameraControlLarge = useRef();
+
+    // model
+    const small = useRef(new THREE.Group());
+    const large = useRef(new THREE.Group());
+
+    // rotation
+    const [smallRotation, setSmallRotation] = useState(0);
+    const [largeRotation, setLargeRotation] = useState(0);
+
     useGSAP(() => {
         gsap.to('heading', {y: 0, opacity: 1 })
     }, []);
@@ -17,7 +38,15 @@ const Model = () => {
 
             <div className="flex flex-col items-center mt-5">
                 <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-                    <ModelView />
+                    <ModelView 
+                       index={1}
+                       groupRef={small}
+                       gsapType="view1"
+                       controlRef={cameraControlSmall}
+                       setRotationState={setSmallRotation}
+                       item={model}
+                       size={size}
+                    />
                 </div>
             </div>
         </div>
