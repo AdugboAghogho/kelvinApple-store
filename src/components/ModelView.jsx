d9d43c2a593eab377967f7731618dc0712ffb081
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-22 12:11:22 +0100

    asx

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Lights from './Lights'
 
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gaspType, controlRef, setRotation, size, item }) => {
+
   return (
-    <div>
-      model
-    </div>
+    <View
+      index={index}
+      id={gaspType}
+      className={`border-2 border-red-200 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+    >
+      {/* ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <Suspense fallback={<div>Loading...</div>} >
+        .
+      </Suspense>
+    </View>
   )
 }
 
