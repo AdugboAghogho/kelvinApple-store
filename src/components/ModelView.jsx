d223408d285b9daa4fd68b462d23e21c36df4ef9
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-22 12:48:34 +0100

    XSeac

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,8 @@
-import { PerspectiveCamera, View } from '@react-three/drei'
+import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei'
 import React, { Suspense } from 'react'
 import Lights from './Lights'
 import IPhone from './IPhone'
+import * as THREE from 'three'
 
 const ModelView = ({ index, groupRef, gaspType, controlRef, setRotation, size, item }) => {
 
@@ -18,9 +19,26 @@ const ModelView = ({ index, groupRef, gaspType, controlRef, setRotation, size, i
 
       <Lights />
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <IPhone />
-      </Suspense>
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target0={new THREE.Vector3(0, 0 ,0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0, 0 ,0]}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+
     </View>
   )
 }
