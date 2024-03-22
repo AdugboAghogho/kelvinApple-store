e824b25cd4d0ffd5c4253d9339d83824ac64e6f0
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-22 12:57:13 +0100

    sxsxz

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -10,7 +10,7 @@ const ModelView = ({ index, groupRef, gaspType, controlRef, setRotation, size, i
     <View
       index={index}
       id={gaspType}
-      className={`w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+      className={`w-full h-full absolute ${index === 2} ? 'right-[-100%] : ''`}
     >
       {/* ambient Light */}
       <ambientLight intensity={0.3} />
