6edbeb4b5cae37c1eac56a91e1259f92fdc3f8d5
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-22 12:52:23 +0100

    sadz

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -14,6 +14,22 @@ function Model(props) {
 
   const texture = useTexture(props.item.img);
 
+  useEffect(() => {
+    Object.entries(materials).map((material) => {
+      // these are the material names that can't be changed color
+      if (
+        material[0] !== "zFdeDaGNRwzccye" &&
+        material[0] !== "ujsvqBWRMnqdwPx" &&
+        material[0] !== "hUlRcbieVuIiOXG" &&
+        material[0] !== "jlzuBkUzuJqgiAK" &&
+        material[0] !== "xNrofRCqOXXHVZt"
+      ) {
+        material[1].color = new THREE.Color(props.item.color[0]);
+      }
+      material[1].needsUpdate = true;
+    });
+  }, [materials, props.item]);
+
   return (
     <group {...props} dispose={null}>
       <mesh
