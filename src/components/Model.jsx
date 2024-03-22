7ad71856eeaff9ce5657593813aa807c3fdae9c1
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-22 14:11:33 +0100

    awsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ModelView from './ModelView';
 import { yellowImg } from '../utils';
 import { Canvas } from '@react-three/fiber';
@@ -8,6 +8,7 @@ import { View } from '@react-three/drei';
 
 import * as THREE from 'three';
 import { models, sizes } from '../constants/index';
+import { animateWithGsapTimeline } from '../utils/animations';
 
 const Model = () => {
     const [size, setSize] = useState('small');
@@ -28,6 +29,26 @@ const Model = () => {
     // rotation
     const [smallRotation, setSmallRotation] = useState(0);
     const [largeRotation, setLargeRotation] = useState(0);
+
+    const tl = gsap.timeline();
+
+    useEffect(() => {
+      if (size === 'large') {
+        animateWithGsapTimeline(tl, small, smallRotation,
+        '#view1', '#view2', {
+          transform: 'translateX(-100%)',
+          duration: 2
+        })
+      }
+
+      if (size === 'small') {
+        animateWithGsapTimeline(tl, large, largeRotation,
+        '#view2', '#view1', {
+          transform: 'translateX(0)',
+          duration: 2
+        })
+      }
+    }, [size])
 
     useGSAP(() => {
         gsap.to('#heading', {y: 0, opacity: 1 })
