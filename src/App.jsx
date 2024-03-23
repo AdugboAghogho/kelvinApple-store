f3cbb649953d4500ced500000b7945ea2cdf133a
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-23 17:06:48 +0100

    yterdsfb

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 
+import  * as Sentry from '@sentry/react';
 
 const App = () => {
+  
   return (
     <main className="bg-black">
       <Navbar />
@@ -15,4 +17,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,14 @@ Sentry.init({
   dsn: "https://2b337f93cb9cefa57d05939df3c8ebae@o4506961017503744.ingest.us.sentry.io/4506961022943232",
   integrations: [
     Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
     Sentry.replayIntegration({
       maskAllText: false,
       blockAllMedia: false,
-    }),
+    }), 
   ],
   tracesSampleRate: 1.0, 
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
