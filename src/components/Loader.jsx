b59f29f097b4cceb49014490bc8ba739b21fc5c8
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-23 15:57:14 +0100

    ssdc

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
         <div className="w-[10vw] h-[10vw] rounded-full">
-          <img src="./public/assets/images/Spinner" alt="" />
+          <img src="/assets/images/Spinner" alt="" />
         </div>
       </div>
     </Html>
