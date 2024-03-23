c47781cde4774df523b16c1341cba4a95781dca5
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-23 17:08:58 +0100

    xc

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./components/Features";
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   )
 }
