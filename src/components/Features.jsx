11d57783961dd029e2860f3f5c4eeeb443a1beb4
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-23 18:45:26 +0100

    asdaesd

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,11 @@
 import { useGSAP } from '@gsap/react'
-import React from 'react'
+import React, { useRef } from 'react'
 import { animateWithGsap } from '../utils/animations'
+import { exploreVideo } from '../utils';
 
 const Features = () => {
+  const videoRef = useRef();  
+
   useGSAP(() => {
     animateWithGsap('#features_title', {
         y: 0,
@@ -22,8 +25,26 @@ const Features = () => {
             </h1>
         </div>
 
-        <div className="flex flex-col">
-            .
+        <div className="flex flex-col justify-center items-center overflow-hidden">
+            <div className="mt-32 mb-24 pl-24">
+                <h2 className="text-5xl lg:text-7xl font-semibold">iPhone</h2>
+                <h2 className="text-5xl lg:text-7xl font-semibold">Forged In Titanium.</h2>
+            </div>
+
+            <div className="flex-center flex-col sm:px-10">
+                <div className="relative h-[50vh] w-full flex items-center">
+                    <video
+                      playsInline
+                      id='exploreVideo'
+                      className='w-full h-full object-cover object-center'
+                      preload='none'
+                      muted
+                      autoPlay ref={videoRef}
+                    >
+                        <source src={exploreVideo} type='video/mp4'/>
+                    </video>
+                </div>
+            </div>
         </div>
       </div>
     </section>
