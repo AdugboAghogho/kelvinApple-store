573ae43ea8b51a910e9b4717b35613e782216fb2
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-23 19:30:35 +0100

    saxc

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,11 +2,23 @@ import { useGSAP } from '@gsap/react'
 import React, { useRef } from 'react'
 import { animateWithGsap } from '../utils/animations'
 import { explore1Img, explore2Img, exploreVideo } from '../utils';
+import gsap from 'gsap';
 
 const Features = () => {
   const videoRef = useRef();  
 
   useGSAP(() => {
+    gsap.to('#exploreVideo', {
+        scrollTrigger: {
+          trigger: '#exploreVideo',
+          toggleActions: 'play pause reverse restart',
+          start: '-10% bottom',
+        },
+        onComplete: () => {
+            videoRef.current.play();
+        }
+      })
+
     animateWithGsap('#features_title', {
         y: 0,
         opacity: 1
@@ -16,6 +28,11 @@ const Features = () => {
         '.g_grow',
         { scale: 1, opacity: 1, ease: 'power1' },
         { scrub: 5.5 }
+    )
+
+    animateWithGsap(
+        '.g_text',
+        { y: 0, opacity: 1, ease: 'power2.inOut', duration: 1 }
     )
   }, [])
 
