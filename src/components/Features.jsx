f60c4f1c567d1e33978c910902b9976883b7d98c
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-23 17:16:05 +0100

    dfcd

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,10 +1,24 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
 
 const Features = () => {
+  useGSAP(() => {
+    
+  }, [])
+
   return (
-    <div>
-      
-    </div>
+    <section className='h-full common-padding bg-zinc relative overflow-hidden'>
+      <div className="screen-max-width">
+        <div className="mb-12 w-full">
+            <h1
+              id='features_title'
+              className='section-heading'
+            >
+                Explore The Full Story
+            </h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
