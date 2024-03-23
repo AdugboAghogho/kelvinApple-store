5e947731f63cbcde7047e04aeb1600eb95dc38c7
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-23 16:06:32 +0100

    cedcd

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,13 @@
 import { Html } from '@react-three/drei'
 import React from 'react'
+import { spinnerImg } from '../utils'
 
 const Loader = () => {
   return (
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
         <div className="w-[10vw] h-[10vw] rounded-full">
-          <img src="/assets/images/Spinner" alt="" />
+          <img src={spinnerImg} alt="spinner" />
         </div>
       </div>
     </Html>
