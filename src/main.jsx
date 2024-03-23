06aadd76cea3ffa959c44f31e445edb7f184e051
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-23 16:40:51 +0100

    sx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,25 @@ import App from './App.jsx'
 import './index.css'
 import './inout.css'
 
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://2b337f93cb9cefa57d05939df3c8ebae@o4506961017503744.ingest.us.sentry.io/4506961022943232",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
