55230d07f27735cbf7b4b9f8b41217af469eb753
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-24 14:16:05 +0100

    vwee

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -55,40 +55,39 @@ const HowItWorks = () => {
               />
             </div>
             <div className="hiw-video">
-                <video className="pointer-events-none" playsInline preload="none" muted autoPlay ref={videoRef}>
-                  <source src={frameVideo} type="video/mp4" />
-                </video>
-              </div>
+              <video className="pointer-events-none" playsInline preload="none" muted autoPlay ref={videoRef}>
+                <source src={frameVideo} type="video/mp4" />
+              </video>
+            </div>
           </div>
           <p className="text-gray font-semibold text-center mt-3">Honkai: Star Rail</p>
+        </div>
+
+        <div className="hiw-text-container">
+          <div className="flex flex-1 justify-center flex-col">
+            <p className="hiw-text g_fadeIn">
+              A17 Pro is an entirely new class of iPhone chip that delivers our {' '}
+              <span className="text-white">
+                best graphic performance by far
+              </span>.
+            </p>
+
+            <p className="hiw-text g_fadeIn mt-10">
+              Mobile {' '}
+              <span className="text-white">
+                games will look and feel so immersive
+              </span>,
+              with incredibly detailed environments and characters.
+            </p>
           </div>
 
-          <div className="hiw-text-container">
-                <div className="flex flex-1 justify-center flex-col">
-                  <p className="hiw-text g_fadeIn">
-                    A17 Pro is an entirely new class of iPhone chip that delivers our {' '}
-                    <span className="text-white">
-                      best graphic performance by far
-                    </span>.
-                  </p>
-
-                  <p className="hiw-text g_fadeIn">
-                   Mobile {' '}
-                    <span className="text-white">
-                      games will look and feel so immersive
-                    </span>,
-                     with incredibly detailed environments and characters.
-                  </p>
-                </div>
-              
-
-              <div className="flex-1 flex justify-center flex-col g_fadeIn">
-                <p className="hiw-text">New</p>
-                <p className="hiw-bigtext">Pro-class GPU</p>
-                <p className="hiw-text">with 6 cores</p>
-              </div>
-              </div>
-            </div>
+          <div className="flex-1 flex justify-center flex-col g_fadeIn">
+            <p className="hiw-text">New</p>
+            <p className="hiw-bigtext">Pro-class GPU</p>
+            <p className="hiw-text">with 6 cores</p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
